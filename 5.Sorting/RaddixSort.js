c4c38bdf31c87a747b2ec7a3c02358957229769c
navneet <navneet.v.maurya@gmail.com>
2023-08-16 19:36:13 +0530

    refactored raddix sort

--- 5.Sorting/RaddixSort.js
+++ 5.Sorting/RaddixSort.js
@@ -1,21 +1,21 @@
-function getDigit(num, i) {
+const getDigit = (num, i) => {
   return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-}
+};
 
-function digitCount(num) {
+const digitCount = (num) => {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
+};
 
-function mostDigits(nums) {
+const mostDigits = (nums) => {
   let maxDigits = 0;
   for (let i = 0; i < nums.length; i++) {
     maxDigits = Math.max(maxDigits, digitCount(nums[i]));
   }
   return maxDigits;
-}
+};
 
-function radixSort(nums) {
+const radixSort = (nums) => {
   let maxDigitCount = mostDigits(nums);
   for (let k = 0; k < maxDigitCount; k++) {
     let digitBuckets = Array.from({ length: 10 }, () => []);
@@ -26,6 +26,6 @@ function radixSort(nums) {
     nums = [].concat(...digitBuckets);
   }
   return nums;
-}
+};
 
 console.log(radixSort([8, 2, 4, 5, 7, 9, 90, 7, 7, 7, 76, 6, 6, 78, 8, 9]));
