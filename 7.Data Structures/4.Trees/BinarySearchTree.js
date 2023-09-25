4088de5c1e46ee0b1cf3f611ad17256e47f13e88
navneet <navneet.v.maurya@gmail.com>
2023-09-25 14:22:20 +0530

    done with BFS in BST

--- 7.Data Structures/4.Trees/BinarySearchTree.js
+++ 7.Data Structures/4.Trees/BinarySearchTree.js
@@ -59,21 +59,43 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  BFS() {
+    const arr = [];
+    const visited = [];
+    let curr;
+    if (this.root) {
+      visited.push(this.root);
+    }
+    while (visited.length > 0) {
+      curr = visited.shift();
+      arr.push(curr.val);
+      if (curr.left) {
+        visited.push(curr.left);
+      }
+      if (curr.right) {
+        visited.push(curr.right);
+      }
+    }
+    return arr;
+  }
 }
 
 const bst = new BinarySearchTree();
 
-console.log(bst.find(10));
-console.log(bst.insert(5));
-console.log(bst.find(6));
-console.log(bst.insert(2));
-console.log(bst.insert(7));
-console.log(bst.insert(7));
+console.log(bst.BFS());
+//console.log(bst.find(10));
+console.log(bst.insert(10));
+//console.log(bst.find(6));
 console.log(bst.insert(6));
+console.log(bst.insert(15));
 console.log(bst.insert(3));
-console.log(bst.find(5));
-console.log(bst.find(2));
-console.log(bst.find(7));
-console.log(bst.find(6));
-console.log(bst.find(3));
-console.log(bst.find(10));
+console.log(bst.insert(8));
+console.log(bst.insert(20));
+//console.log(bst.find(5));
+//console.log(bst.find(2));
+//console.log(bst.find(7));
+//console.log(bst.find(6));
+//console.log(bst.find(3));
+//console.log(bst.find(10));
+console.log(bst.BFS());
